5078b5b842ca9942308f55477a9f1ba700b700cd
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2015-05-14 14:46:29 +0200

    change the order of IPv6 regexps
    This fixes my failing tests.

--- hosts.tmLanguage.rb
+++ hosts.tmLanguage.rb
@@ -32,20 +32,25 @@ PRIVATE_IP_V4 = [
 
 # Luckily David M. Syzdek already did the hard work and we can use it here
 # http://stackoverflow.com/a/17871737/852657
+#
+# If you ever need to change these regexps, take care with reordering the array.
+# It is really easy to make a mistake and quite hard to figure out why things don't work like intended.
 SEGMENT = "[0-9a-fA-F]{1,4}"
 IP_V6 = [
-    "(#{SEGMENT}:){7,7}#{SEGMENT}",             # 1:2:3:4:5:6:7:8
-    "(#{SEGMENT}:){1,7}:",                      # 1::                                 1:2:3:4:5:6:7::
-    "(#{SEGMENT}:){1,6}:#{SEGMENT}",            # 1::8               1:2:3:4:5:6::8   1:2:3:4:5:6::8
-    "(#{SEGMENT}:){1,5}(:#{SEGMENT}){1,2}",     # 1::7:8             1:2:3:4:5::7:8   1:2:3:4:5::8
-    "(#{SEGMENT}:){1,4}(:#{SEGMENT}){1,3}",     # 1::6:7:8           1:2:3:4::6:7:8   1:2:3:4::8
-    "(#{SEGMENT}:){1,3}(:#{SEGMENT}){1,4}",     # 1::5:6:7:8         1:2:3::5:6:7:8   1:2:3::8
-    "(#{SEGMENT}:){1,2}(:#{SEGMENT}){1,5}",     # 1::4:5:6:7:8       1:2::4:5:6:7:8   1:2::8
-    "#{SEGMENT}:((:#{SEGMENT}){1,6})",          # 1::3:4:5:6:7:8     1::3:4:5:6:7:8   1::8
-    ":((:#{SEGMENT}){1,7}|:)",                  # ::2:3:4:5:6:7:8    ::2:3:4:5:6:7:8  ::8       ::
-    "fe80:(:#{SEGMENT}){0,4}%[0-9a-zA-Z]{1,}",  # fe80::7:8%eth0     fe80::7:8%1  (link-local IPv6 addresses with zone index)
     "::(ffff(:0{1,4}){0,1}:){0,1}#{IP_V4}",     # ::255.255.255.255  ::ffff:255.255.255.255  ::ffff:0:255.255.255.255 (IPv4-mapped IPv6 addresses and IPv4-translated addresses)
-    "(#{SEGMENT}:){1,4}:#{IP_V4}"               # 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33 (IPv4-Embedded IPv6 Address)
+
+    "fe80:(:#{SEGMENT}){0,4}\%[0-9a-zA-Z]{1,}",  # fe80::7:8%eth0     fe80::7:8%1  (link-local IPv6 addresses with zone index)
+
+    "(#{SEGMENT}:){7,7}#{SEGMENT}",             # 1:2:3:4:5:6:7:8
+    ":((:#{SEGMENT}){1,7}|:)",                  # ::2:3:4:5:6:7:8    ::2:3:4:5:6:7:8  ::8       ::
+    "(#{SEGMENT}:){1,4}:#{IP_V4}",              # 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33 (IPv4-Embedded IPv6 Address)
+    "#{SEGMENT}:((:#{SEGMENT}){1,6})",          # 1::3:4:5:6:7:8     1::3:4:5:6:7:8   1::8
+    "(#{SEGMENT}:){1,2}(:#{SEGMENT}){1,5}",     # 1::4:5:6:7:8       1:2::4:5:6:7:8   1:2::8
+    "(#{SEGMENT}:){1,3}(:#{SEGMENT}){1,4}",     # 1::5:6:7:8         1:2:3::5:6:7:8   1:2:3::8
+    "(#{SEGMENT}:){1,4}(:#{SEGMENT}){1,3}",     # 1::6:7:8           1:2:3:4::6:7:8   1:2:3:4::8
+    "(#{SEGMENT}:){1,5}(:#{SEGMENT}){1,2}",     # 1::7:8             1:2:3:4:5::7:8   1:2:3:4:5::8
+    "(#{SEGMENT}:){1,6}:#{SEGMENT}",            # 1::8               1:2:3:4:5:6::8   1:2:3:4:5:6::8
+    "(#{SEGMENT}:){1,7}:",                      # 1::                                 1:2:3:4:5:6:7::
   ].map { |variant| "(#{variant})" }.join('|')
 
 
